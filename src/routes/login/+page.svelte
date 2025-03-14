03ac05d992c23502db6d3ebabe154e8e58b8c370
Erlend Johann Skarpnes <erlend.skarpnes@miles.no>
2025-03-14 14:00:10 +0100

    landing page info

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
-
-	import { Button, Card, Input, Label } from 'flowbite-svelte';
+	import { Button, Card, Heading, Input, Label } from 'flowbite-svelte';
 </script>
 
-<div class="flex justify-center items-center px-4 py-2 h-screen">
+<div class="flex flex-col gap-8 justify-center items-center px-4 py-2 h-screen">
+	<Heading class="text-center">Bryggetrackern</Heading>
+	<p class="text-gray-300 w-100 text-center">Dette er en alternativ frontend til BrewCreator som jeg bruker når jeg brygger.
+		Innloggingen som benyttes på BrewCreator vil også fungere her.
+		Løsningen lagrer ingen data, og lagrer kun tokenet for kommunikasjon med BrewCreator i en cookie.</p>
 	<Card>
 	<form method="POST" class="flex flex-col gap-4">
 		<div>
